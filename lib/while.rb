9c5f4de101c2cf184a9bd94554225fe7fae443d3
Andrew Williams <1williamsaj@gmail.com>
2019-08-22 00:53:43 +0000

    Done.

--- lib/while.rb
+++ lib/while.rb
@@ -1,7 +1,10 @@
 def using_while
-  levitation_force = 6
-	
-  #your code here
+  levitation_force = 6 #Think of this as our counter. Instead of 0, we start at 6.
+
+
+  while levitation_force < 10 #We open our while loop by saying "While variable is less than 10, execute the block below."
+    puts "Wingardium Leviosa" #The block outputs this phrase.
+    levitation_force += 1 #The value of our variable is reassigned to it's value plus 1. Then the loop repeats until the condition is met.
+  end
+
 end
-
-
