55000d2732410d3c6ed4e961f2ff0438a3057d93
Edward Kane <edwarde.kane@gmail.com>
2020-02-19 15:04:14 +0000

    Done.

--- lib/while.rb
+++ lib/while.rb
@@ -1,7 +1,11 @@
 def using_while
+  require_relative "../lib/while.rb"
+  counter=0
   levitation_force = 6
-	
-  #your code here
+
+  while counter <10
+    puts "puts out the phrase 'Wingardium Leviosa' while the levitation force is incremented to 10"
+    counter += 1
+    break
 end
-
-
+puts "And the Loop is Done"
