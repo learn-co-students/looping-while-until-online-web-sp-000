880ba93f6269e22d03c19aa0ebc69001e8adf244
Mark Stabler <mark@stablerwritingservices.com>
2020-02-20 16:15:01 +0000

    Done.

--- lib/until.rb
+++ lib/until.rb
@@ -1,6 +1,8 @@
 def using_until
-  levitation_force = 6
+  lev_force = 6
   #your code here
-    
+  until lev_force == 10
+	   puts "Wingardium Leviosa"
+	    lev_force += 1
+  end
 end
-
--- lib/while.rb
+++ lib/while.rb
@@ -1,7 +1,9 @@
 def using_while
-  levitation_force = 6
-	
+  lev_force = 6
+
   #your code here
+  while lev_force < 10
+    puts "Wingardium Leviosa"
+    lev_force += 1
+  end
 end
-
-
